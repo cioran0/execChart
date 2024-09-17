c98e9ff9a0c6a2e0b02bceed38998e5f66e5b4a4
cioran0 <michaelmurdoch0@gmail.com>
2024-09-17 00:51:00 -0400

    .

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+//https://towardsdev.com/chart-js-next-js-beautiful-data-driven-dashboards-how-to-create-them-fast-and-efficiently-a59e313a3153
+//import controllers elemets etc 
+
+import{
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+    Filler,
+} from "chart.js";
+
+//register them
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Filler,
+    Title,
+    Tooltip,
+    Legend
+);
+
 import { Bar, Line, Scatter, Bubble } from  "react-chartjs-2"
